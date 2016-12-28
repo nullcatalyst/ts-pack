de5e9b5a6afe78ed886ceaf64418c5be6f67ba9f
Scott Bennett <scottbennett912@gmail.com>
2016-12-28 01:03:44 -0600

    Initial commit

---
+++ test/parse.js
@@ -0,0 +1,9 @@
+const assert = require('assert');
+
+describe('ts', function () {
+    describe('#test()', function () {
+        it('should do something', function () {
+            
+        });
+    });
+});
